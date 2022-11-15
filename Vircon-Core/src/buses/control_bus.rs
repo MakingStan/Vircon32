0b13239ae20eaa64f915ef1e327c641a026bec56
MakingStan <78844475+MakingStan@users.noreply.github.com>
2022-11-15 17:31:28 +0100

    Properly made control_bus

--- Vircon-Core/src/buses/control_bus.rs
+++ Vircon-Core/src/buses/control_bus.rs
@@ -9,25 +9,27 @@ use crate::components::vircon_component::VirconComponent;
 
 
 pub struct ControlBus {
-    slaves: [dyn VirconComponent; 8],
+    gpu: Gpu,
+    spu: Spu,
+    rng: Rng,
+    cartridge_controller: CartridgeController,
+    timer: Timer,
+    gamepad_controller: GamePadController,
+    memory_card_controller: MemoryCardController,
 }
 
 impl ControlBus {
     pub fn new() -> ControlBus
     {
-        let control_bus =  ControlBus {
-            slaves: [0; 8],
-        };
-
-        slaves[0] = Timer::new();
-        slaves[1] = Rng::new();
-        slaves[2] = Gpu::new();
-        slaves[3] = Spu::new();
-        slaves[4] = GamePadController::new();
-        slaves[5] = CartridgeController::new();
-        slaves[6] = MemoryCardController::new();
-
-        return control_bus;
+        return ControlBus {
+            gpu: Gpu::new(),
+            spu: Spu::new(),
+            rng: Rng::new(),
+            cartridge_controller: CartridgeController::new(),
+            timer: Timer::new(),
+            gamepad_controller: GamePadController::new(),
+            memory_card_controller: MemoryCardController::new()
+        }
     }
 
     pub fn read_port(&mut self, global_port: i32, mut result: &mut i32) -> bool
@@ -36,7 +38,8 @@ impl ControlBus {
         let device_id: i32 = (global_port >> 8) & 7;
         let local_port: i32 = global_port & 0xFF;
 
-        let succes: bool =  self.slaves[device_id].read_port(local_port, &mut result);
+        let mut device = self.device_id_to_slave(device_id);
+        let succes: bool =  device.read_port(local_port, &mut result);
 
         return succes;
         //Don't handle hardware errors for now
@@ -48,11 +51,42 @@ impl ControlBus {
         let device_id: i32 = (global_port >> 8) & 7;
         let local_port: i32 = global_port & 0xFF;
 
-
-        let success: bool = self.slaves[device_id].write_port(local_port, value);
+        let mut device = self.device_id_to_slave(device_id);
+        let success: bool = device.write_port(local_port, value);
 
         return success;
 
         //Don't handle hardware errors for now
+    }
+
+    fn device_id_to_slave(&mut self, device_id: i32) -> Box<&dyn VirconComponent>
+    {
+        match device_id {
+            0 => {
+                return Box::new(&self.timer);
+            }
+            1 => {
+                return Box::new(&self.rng);
+            }
+            2 => {
+                return Box::new(&self.gpu);
+            }
+            3 => {
+                return Box::new(&self.spu);
+            }
+            4 => {
+                return Box::new(&self.gamepad_controller);
+            }
+            5 => {
+                return Box::new(&self.cartridge_controller);
+            }
+            6 => {
+                return Box::new(&self.memory_card_controller);
+            }
+            _ => {
+                //should not exist
+                panic!("Device does not exist.");
+            }
+        }
     }
 }
